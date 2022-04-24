1f07842d1ff5f74d9e440e5f3f208213561050f3
Фаддей Гусаров <gusarovfg@gmail.com>
2022-04-24 16:05:03 +0400

    feat: В проект добавлена сторонняя библиоткеа SnapKit

---
+++ RickAndMortyTestApp/ViewControllers/DetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewController.swift
+//  RickAndMortyTestApp
+//
+//  Created by Фаддей Гусаров on 24.04.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var character: Character?
+    
+    var characterImageView: UIImageView?
+    var characterDescription: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
